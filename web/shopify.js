47a378ab95d5eebbb92634590ae64355de4fa2c0
CarlosCardenas24 <nooptions101@gmail.com>
2023-06-22 18:35:46 -0700

    backend for QR codes done

--- web/shopify.js
+++ web/shopify.js
@@ -2,8 +2,10 @@ import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
+import sqlite3 from "sqlite3";
+import { join } from "path";
 
-const DB_PATH = `${process.cwd()}/database.sqlite`;
+import { QRCodesDB } from "./qr-codes-db.js";
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
@@ -16,6 +18,10 @@ const billingConfig = {
   },
 };
 
+const database = new sqlite3.Database(join(process.cwd(), "database.sqlite"));
+// Initialize SQLite DB
+QRCodesDB.db = database;
+QRCodesDB.init();
 const shopify = shopifyApp({
   api: {
     apiVersion: LATEST_API_VERSION,
@@ -29,8 +35,7 @@ const shopify = shopifyApp({
   webhooks: {
     path: "/api/webhooks",
   },
-  // This should be replaced with your preferred storage strategy
-  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+  sessionStorage: new SQLiteSessionStorage(database),
 });
 
 export default shopify;
