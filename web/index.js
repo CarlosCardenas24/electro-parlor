7e52508892326af4b8189a60d1d35d80281dbeea
CarlosCardenas24 <nooptions101@gmail.com>
2023-06-27 15:43:29 -0700

    Shopify QR Tutorial done

--- web/index.js
+++ web/index.js
@@ -8,6 +8,8 @@ import shopify from "./shopify.js";
 import GDPRWebhookHandlers from "./gdpr.js";
 
 import applyQrCodeApiEndpoints from "./middleware/qr-code-api.js";
+import applyQrCodePublicEndpoints from "./middleware/qr-code-public.js";
+
 
 const PORT = parseInt(
   process.env.BACKEND_PORT || process.env.PORT || "3000",
@@ -35,6 +37,8 @@ app.post(
 
 // If you are adding routes outside of the /api path, remember to
 // also add a proxy rule for them in web/frontend/vite.config.js
+
+applyQrCodePublicEndpoints(app);
 
 app.use("/api/*", shopify.validateAuthenticatedSession());
 
